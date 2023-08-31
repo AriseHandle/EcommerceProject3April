509f77d6ab047d7ba5e38670f411b0a1631bc6bd
Karali <karalibaviskar5@gmail.com>
2023-08-31 11:58:51 +0530

    update the product specification page

--- src/main/java/pom/ProductSpecificationPage.java
+++ src/main/java/pom/ProductSpecificationPage.java
@@ -1,5 +1,40 @@
 package pom;
 
-public class ProductSpecificationPage {
+import java.util.List;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductSpecificationPage {
+	@FindBy (xpath="//a[@class='sml-link tooltip']") private WebElement wishList;
+	@FindBy (xpath="//a[@id='cart-panel-button-0']") private WebElement clickHereToBuy;
+	@FindBy (xpath="//a[@class='terms_condition']") private WebElement privacyPolicy;
+	@FindBy(xpath="//input[@id='pincodeDeliveryId_0']") private WebElement pinCode;
+	@FindBy(xpath="//a[@class='button_1'][1]") private WebElement checkButton;
+	
+	public ProductSpecificationPage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void clickOnWishList() {
+		wishList.click();
+	}
+	
+	public void clickOnClickHereToBuy() {
+		clickHereToBuy.click();
+	}
+	
+	public void clickOnPrivacyPolicy() {
+		privacyPolicy.click();
+	}
+	
+	public void enterPinCode(String a) {
+		pinCode.sendKeys(a);
+	}
+	public void clickOnCheckButton() {
+		checkButton.click();
+	}
 }
